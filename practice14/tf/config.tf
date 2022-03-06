7aace6d1e49a64da6df1834c927cf1c1f3f2011f
aliev-mark <withoutspleen@gmail.com>
2022-03-06 15:34:44 +0300

    edit config.tf

--- practice14/tf/config.tf
+++ practice14/tf/config.tf
@@ -8,10 +8,28 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("gcp-creds.json")
+  credentials = file("~/.gcp/gcp-creds.json")
   project     = "master-imagery-335207"
   region      = "us-central1"
   zone        = "us-central1-a"
+}
+
+resource "google_project_service" "api" {
+  for_each = toset ([
+   " serviceusage.googleapis.com " ])
+  disable_on_destroy = false
+  service = each.value
+}
+
+resource "google_compute_firewall" "tomcat" {
+  name    = "tomcat-access"
+  network = "default"
+  source_ranges = ["0.0.0.0/0"]
+  allow {
+    protocol = "tcp"
+    ports = ["8080", "443", "22", "80"]
+  }
+
 }
 
 resource "google_compute_instance" "test" {
@@ -23,8 +41,18 @@ resource "google_compute_instance" "test" {
       image = "ubuntu-os-cloud/ubuntu-2004-lts"
     }
   }
-
   network_interface {
     network = "default"
   }
 }
+metadata_startup_script = <<EOF
+#!/bin/bash
+apt update -y
+apt install maven git -y
+cd /tmp/
+git clone https://github.com/boxfuse/boxfuse-sample-java-war-hello.git
+cd boxfuse-sample-java-war-hello
+mvn package
+EOF
+
+depends_on = [google_project_service.api, google_compute_firewall.web]
