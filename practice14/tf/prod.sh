5d6bf64d80716da099fca6379e14de429f031e9d
aliev-mark <withoutspleen@gmail.com>
2022-03-09 19:42:22 +0300

    edit config.tf

--- practice14/tf/prod.sh
+++ practice14/tf/prod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt update
-apt install tomcat -y
+apt install tomcat9 -y
 apt install apt-transport-https ca-certificates gnupg -y
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
@@ -8,4 +8,4 @@ apt update
 apt install google-cloud-cli -y
 yes | gcloud auth login --cred-file=/tmp/gcp-creds.json
 gsutil cp gs://test-bucket-practice/hello-1.0.war /var/lib/tomcat9/webapps/
-service tomcat restart
+service tomcat9 restart
