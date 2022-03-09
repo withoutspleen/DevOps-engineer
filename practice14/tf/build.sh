b775f3d4dbddc9940a655581d03f938961c1a27f
aliev-mark <withoutspleen@gmail.com>
2022-03-09 17:56:30 +0300

    edit config.tf

--- practice14/tf/build.sh
+++ practice14/tf/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-apt update
-apt install maven git -y
+sudo apt update
+sudo apt install maven git -y
 cd /tmp
-git clone https://github.com/boxfuse/boxfuse-sample-java-war-hello.git
-cd boxfuse-sample-java-war-hello
-mvn package
-apt install apt-transport-https ca-certificates gnupg -y
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-apt update
-apt install google-cloud-cli -y
-yes | gcloud auth login --cred-file=/tmp/gcp-creds.json
-gsutil cp target/hello-1.0.war gs://test-bucket-practice/
+sudo git clone https://github.com/boxfuse/boxfuse-sample-java-war-hello.git
+sudo cd boxfuse-sample-java-war-hello
+sudo mvn package
+sudo apt install apt-transport-https ca-certificates gnupg -y
+sudo echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt update
+sudo apt install google-cloud-cli -y
+sudo yes | gcloud auth login --cred-file=/tmp/gcp-creds.json
+sudo gsutil cp target/hello-1.0.war gs://test-bucket-practice/
