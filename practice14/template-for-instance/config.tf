5b87ffb457fd6511675479ace45a527f9cbcb265
mark-work <withoutspleen@gmail.com>
2022-03-10 11:16:16 +0300

    edit config.tf

--- practice14/template-for-instance/config.tf
+++ practice14/template-for-instance/config.tf
@@ -35,5 +35,5 @@ resource "google_compute_instance" "practice" {
 }
 
 output "instance_ip_address" {
-  value = google_compute_instance[0].network_interface[0].access_config[0].nat_ip
+  value = google_compute_instance.practice.network_interface[0].access_config[0].nat_ip
 }
