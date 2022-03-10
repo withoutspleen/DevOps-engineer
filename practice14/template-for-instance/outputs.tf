ca88df83b77c95d1d935430b425ac31b23dc45ad
mark-work <withoutspleen@gmail.com>
2022-03-10 11:31:41 +0300

    edit output.tf

--- practice14/template-for-instance/outputs.tf
+++ practice14/template-for-instance/outputs.tf
@@ -1,4 +1,4 @@
 output "instance_ip_address" {
-  description = "Instance public IP address is"
+  description = "Instance public IP address"
   value = google_compute_instance.practice.network_interface[0].access_config[0].nat_ip
 }
