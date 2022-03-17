c4cbeeb538c56348a949320e5691f98b31d6b8c9
mark-work <withoutspleen@gmail.com>
2022-03-17 15:12:19 +0300

    edit config.tf

--- certification/terraform/config.tf
+++ certification/terraform/config.tf
@@ -9,7 +9,10 @@ terraform {
 
 provider "aws" {
   shared_credentials_file = "~/.aws/credentials"
-  region      = "us-east-1"
+  region                  = "us-east-1"
 }
 
-
+resource "aws_instance" "build" {
+  ami           = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+}
