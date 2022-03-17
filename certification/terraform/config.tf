c5b9c3bc901008576593ae0d0a7f5b5d1c702172
mark-work <withoutspleen@gmail.com>
2022-03-17 14:53:48 +0300

    add certification

---
+++ certification/terraform/config.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~ 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  shared_credentials_file = "~/.aws/credentials"
+  region      = "us-east-1"
+}
+
+
