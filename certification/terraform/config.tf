5e8ba8682cb2fb70d616ee8f2e689d30f07748e6
mark-work <withoutspleen@gmail.com>
2022-03-17 16:34:08 +0300

    edit config.tf

--- certification/terraform/config.tf
+++ certification/terraform/config.tf
@@ -43,7 +43,7 @@ resource "aws_instance" "build" {
   ami           = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
   key_name      = "aws-key"
-  security_groups = [build-prod]
+  security_groups = ["build-prod"]
   tags = {
     name        = "build"
   }
