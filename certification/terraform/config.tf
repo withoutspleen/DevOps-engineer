428547cd277ce1630c67a53a6d61938e685a207a
mark-work <withoutspleen@gmail.com>
2022-03-17 16:57:31 +0300

    edit config.tf

--- certification/terraform/config.tf
+++ certification/terraform/config.tf
@@ -25,12 +25,14 @@ resource "aws_security_group" "build-prod" {
     from_port = 22
     protocol  = "tcp"
     to_port   = 22
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port = 0
     protocol  = "-1"
     to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
 
