1fbc5bfcf9d13a8b1023645fab7ecb6c1243b05a
mark-work <withoutspleen@gmail.com>
2022-03-17 15:56:38 +0300

    edit config.tf

--- certification/terraform/config.tf
+++ certification/terraform/config.tf
@@ -21,4 +21,7 @@ resource "aws_instance" "build" {
   ami           = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
   key_name      = "aws-key"
+  tags = {
+    name        = "build"
+  }
 }
