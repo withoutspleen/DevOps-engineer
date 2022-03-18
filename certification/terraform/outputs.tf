63f68878348a2e5fa72c66474333253269814d31
mark-work <withoutspleen@gmail.com>
2022-03-18 15:31:36 +0300

    edit outputs.tf

--- certification/terraform/outputs.tf
+++ certification/terraform/outputs.tf
@@ -1,9 +1,9 @@
-output "build_instance_ip_address" {
+output "build_instance_dns" {
   description = "Build instance public IP address"
-  value = aws_instance.build.public_ip
+  value = aws_instance.build.public_dns
 }
 
-output "prod_instance_ip_address" {
+output "prod_instance_dns" {
   description = "Production instance public IP address"
-  value = aws_instance.prod.public_ip
+  value = aws_instance.prod.public_dns
 }
