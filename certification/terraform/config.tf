f926ea92e66af095cc12810adc7fbb19ed1a17e2
mark-work <withoutspleen@gmail.com>
2022-03-18 11:07:54 +0300

    edit config.tf

--- certification/terraform/config.tf
+++ certification/terraform/config.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
 resource "aws_key_pair" "aws-key" {
   key_name   = "aws-key"
-  public_key = "$file(/home/user/.aws/aws-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCkEdgGpQHy8bbVasLvSfg6qFRsreci75X/rqDCe5eK+P6fQf1q1T6ML3Nd9AGpikoPuENfoyEaE5+xUiO3bRPwJBaDbqnFE1xFFfTpg5O2MWWzogFIU0ruOEqg0tOlC1kagP69SnqVT+wH4ZINxlawIYlbhgAQsDzvm9AVlz+Lrdsx2z4+1x6jrnXMffF9BIRnQ8Y31owyUFJagHL0iVWivZv/Ls71Ugv6hjk6TaiH7bSahk/Emm1pZxMLtO5l1x55Sh4KoegIqYwZrbA/6mHOF9qyAeIczVLjrquTxJlkS3QA/zuTfAjrTYKZ8W9bdxm6ZmxBxsdc8LH/mWAWbqG8lTUOCQsSk3XAl8kZkc9QSzITza/ATgPMSxMw2EQ0UO8xWjAJJixA1UTTJyfmbUhYxVVbovmvuqIbqopBrbvPMb+uoGGuakku5l/PyQyJHOpkKOW6X9WEjDfbM8F8SlzPSXpQ0JiLcj2zBb1luG3xuGwTbvD/HjgxgkrWzEWdgGi70FwQCBSauncWzA7dfvA10cacT4PZpAQp5haZH6bie0qHZ16f5xsmGNmExxSfvQJ76CZJIFQva32f8+//VmfxBpfRVsUhf21/e5JyArnwCo0/jGDtYAmHZBZLgd7E9Gp3WYlLHLwcNHeT8UXWfhpxiEwIeUetqnRrUEnfqNf4Gw== aws"
 }
 
 resource "aws_security_group" "build-prod" {
