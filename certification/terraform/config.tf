2df20499587978465c142b26847aeddc6d6a22a0
aliev-mark <withoutspleen@gmail.com>
2022-03-23 23:19:18 +0300

    fix inbound ports

--- certification/terraform/config.tf
+++ certification/terraform/config.tf
@@ -25,7 +25,7 @@ resource "aws_security_group" "build-prod" {
   ingress {
     from_port = 22
     protocol  = "tcp"
-    to_port   = 80
+    to_port   = 8080
     cidr_blocks = ["0.0.0.0/0"]
   }
 
